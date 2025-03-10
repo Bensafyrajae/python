09e1d2889ea09936c1ae9f5a336c72960426f327
Bensafyrajae <bensafyrajae19@gmail.com>
2025-03-10 13:34:36 +0000

    Exercice3

---
+++ Exercice3.py
@@ -0,0 +1,7 @@
+my_name = "Rajae"
+user_name = input("What is your name? ")
+
+if user_name == my_name:
+    print("Hey, we have the same name!")
+else:
+    print(f"Nice to meet you, {user_name}! I'm {my_name}, but you already knew that, right?")
