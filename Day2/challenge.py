36cc2d0e2b065af217dbcbd4b4ba6156ed1c04c2
Bensafyrajae <bensafyrajae19@gmail.com>
2025-03-12 09:49:08 +0000

    challenge

---
+++ Day2/challenge.py
@@ -0,0 +1,10 @@
+word = input("Entrez un mot : ")
+
+letter_indexes = {}
+
+for index, letter in enumerate(word):
+    if letter not in letter_indexes:
+        letter_indexes[letter] = []
+    letter_indexes[letter].append(index)
+
+print(letter_indexes)
