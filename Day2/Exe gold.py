d4617c681ec2b554a81e1442dd606ae1be4652a0
Bensafyrajae <bensafyrajae19@gmail.com>
2025-03-12 14:08:50 +0000

    exercice 2 gold day 2

--- Day2/Exe gold.py
+++ Day2/Exe gold.py
@@ -13,3 +13,15 @@ if name in birthdays:
     print(f"{name}'s birthday is on {birthdays[name]}.")
 else:
     print(f"Sorry, we don't have {name}'s birthday in the list.")
+#Exercice2
+print("Welcome to the Birthday Look-up Program!")
+print("Here are the names in our database:")
+for name in birthdays.keys():
+    print("-", name)
+
+name = input("Enter a person's name to find their birthday: ")
+
+if name in birthdays:
+    print(f"{name}'s birthday is on {birthdays[name]}.")
+else:
+    print(f"Sorry, we don’t have the birthday information for {name}.")
